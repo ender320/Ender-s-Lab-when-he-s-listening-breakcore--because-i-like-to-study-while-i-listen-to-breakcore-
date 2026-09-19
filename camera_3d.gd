bb8d597fc160ab72f5ffbba1915e1c9201945670
Everton Vinicius <vinicius.gamed@gmail.com>
2026-09-19 01:08:23 -0300

    experimentos 3d que eu vi no youtube

---
+++ camera_3d.gd
@@ -0,0 +1,19 @@
+extends Camera3D
+
+# Variáveis configuráveis no Inspetor
+@export var raio: float = 5.0       # Tamanho do círculo
+@export var velocidade: float = 0.5 # Velocidade do movimento
+
+var tempo: float = 0.0
+
+func _process(delta):
+	# Acumula o tempo decorrido
+	tempo += delta * velocidade
+	
+	# Calcula as novas posições X e Z usando seno e cosseno
+	var x: float = cos(tempo) * raio
+	var z: float = sin(tempo) * raio
+	rotate_y(-0.18 * delta)
+	
+	# Aplica a nova posição (mantendo a altura Y original)
+	position = Vector3(x, position.y, z)
