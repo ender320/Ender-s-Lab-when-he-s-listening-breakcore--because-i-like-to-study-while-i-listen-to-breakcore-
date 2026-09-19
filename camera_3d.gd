fa24e199b283f8a945ea51ae9c8b2f10e75a45ba
Everton Vinicius <vinicius.gamed@gmail.com>
2026-09-19 01:29:59 -0300

    Update camera_3d.gd

--- camera_3d.gd
+++ camera_3d.gd
@@ -7,14 +7,11 @@ extends Camera3D
 var tempo: float = 0.0
 
 func _process(delta):
-	# Acumula o tempo decorrido
 	tempo += delta * velocidade
 	
-	# Calcula as novas posições X e Z usando seno e cosseno
 	var x: float = cos(tempo) * raio
 	var z: float = sin(tempo) * raio
 	rotate_y(-0.2 * delta)
 	rotate_x(-0.2 * delta)
 	
-	# Aplica a nova posição (mantendo a altura Y original)
 	position = Vector3(x, position.y, z)
